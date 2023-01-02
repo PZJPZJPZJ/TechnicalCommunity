4fc6c381d28223d1f0583f13ff25d938dba36c24
PZJ <68857304+PZJPZJPZJ@users.noreply.github.com>
2023-01-02 17:57:04 +0800

    init

---
+++ src/main/java/com/pzj/technicalcommunity/TechnicalCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.pzj.technicalcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pzj/technicalcommunity/TechnicalCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pzj.technicalcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
