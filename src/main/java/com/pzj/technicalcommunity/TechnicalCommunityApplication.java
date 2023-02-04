e3af0dad0273f7998bcbcd4dfda3022858b30218
PZJ <11253705+pzjpzjpzj@user.noreply.gitee.com>
2023-02-04 18:51:49 +0800

    生成器代码测试通过

--- src/main/java/com/pzj/technicalcommunity/TechnicalCommunityApplication.java
+++ src/main/java/com/pzj/technicalcommunity/TechnicalCommunityApplication.java
@@ -1,9 +1,11 @@
 package com.pzj.technicalcommunity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.pzj.technicalcommunity.mapper")
 public class TechnicalCommunityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pzj/technicalcommunity/controller/TcUserController.java
+++ src/main/java/com/pzj/technicalcommunity/controller/TcUserController.java
@@ -1,9 +1,15 @@
 package com.pzj.technicalcommunity.controller;
 
 
+import com.pzj.technicalcommunity.entity.TcUser;
+import com.pzj.technicalcommunity.service.ITcUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2023-02-04
  */
 @RestController
-@RequestMapping("/tc-user")
+@RequestMapping("/user")
 public class TcUserController {
-
+    @Autowired
+    private ITcUserService iTcUserService;
+    @GetMapping("/list")
+    public List<TcUser> list(){
+        return iTcUserService.list();
+    }
 }
