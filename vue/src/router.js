d4bc21d13821630e97daba977e4bfbeff2d6b6f3
PZJ <11253705+pzjpzjpzj@user.noreply.gitee.com>
2023-02-15 19:06:24 +0800

    后端未实现删除关联表项,验证用户密码，密码加密，JWT，Security 前端完成页面路由

--- vue/src/router.js
+++ vue/src/router.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import SignIn from "@/views/Login.vue";
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
 
  const router = createRouter({
      history: createWebHistory(import.meta.env.BASE_URL),
      routes: [
          {
              path:'/',
-             name:'home',
-             component:SignIn
+             name:'Home',
+             component:Home
          },
+         {
+             path:'/login',
+             name:'Login',
+             component:Login
+         }
      ]
- })
+ });
+ export default router;
